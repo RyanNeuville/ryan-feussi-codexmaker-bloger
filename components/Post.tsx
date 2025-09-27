e3506d6f16a7772e38f300ebb9248e352054c819
Ryan Neuville <neuvilleryan123@gmail.com>
2025-09-27 04:34:16 +0100

    Add next.config.js to allow images pattern urls

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Router from "next/router";
 import ReactMarkdown from "react-markdown";
+import Image from "next/image";
 
 export type PostProps = {
   id: string;
@@ -8,6 +9,7 @@ export type PostProps = {
   author: {
     name: string;
     email: string;
+    image: string;
   } | null;
   content: string;
   published: boolean;
@@ -15,9 +17,14 @@ export type PostProps = {
 
 const Post: React.FC<{ post: PostProps }> = ({ post }) => {
   const authorName = post.author ? post.author.name : "Unknown author";
+  const authorImage = "https://e7.pngegg.com/pngimages/84/165/png-clipart-united-states-avatar-organization-information-user-avatar-service-computer-wallpaper-thumbnail.png"
   return (
     <div onClick={() => Router.push("/p/[id]", `/p/${post.id}`)}>
-      <h2>{post.title}</h2>
+      <span className="text-sm md:text-md lg:text-xl xl:text-2xl font-mono font-bold text-gray-800/50">TECH</span>
+      <h2 className="text-xl md:text-2xl lg:text-3xl xl:text-4xl font-bold text-blue-600 mb-2">{post.title}</h2>
+      <div>
+        <Image src={authorImage} alt="user picture" width={40} height={20}></Image>
+      </div>
       <small>By {authorName}</small>
       <ReactMarkdown children={post.content} />
       <style jsx>{`
---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  images: {
+    remotePatterns: [
+      new URL("https://e7.pngegg.com/**"),
+      new URL("https://avatars.githubusercontent.com/**"),
+    ],
+  },
+};
